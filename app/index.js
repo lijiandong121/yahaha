68772e2e69803cd044d88ae1e6271a2989c8959b
lijiandong <lijiandong@100tal.com>
2018-06-23 13:37:59 +0800

    second

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 var sub = require('./sub');
 var app = document.createElement('div');
-app.innerHTML = '<h1>Hello World</h1>';
+app.innerHTML = '<h1>Hello World!</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
